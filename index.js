46fb82df702b885e66f832e6a955502eb4c1b4b0
xuwei <starkcoder@sina.com>
2020-11-08 18:01:43 +0800

    引用

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*
  * @Author: xuxiaowei
  * @Date: 2020-11-08 00:50:46
- * @LastEditTime: 2020-11-08 00:54:44
- * @LastEditors: xuxiaowei
+ * @LastEditTime: 2020-11-08 17:58:59
+ * @LastEditors: xuwei
  * @Description:
  */
-import Picker from "./src/index";
+import { Picker } from "./src/index";
 
 export const CascadePicker = Picker;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 /*
  * @Author: xuxiaowei
  * @Date: 2020-11-04 12:24:42
- * @LastEditTime: 2020-11-08 00:35:51
- * @LastEditors: xuxiaowei
+ * @LastEditTime: 2020-11-08 18:00:28
+ * @LastEditors: xuwei
  * @Description:
  */
-import React, {PureComponent} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, { PureComponent } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 // import provinceMap from './province.json';
-import {SingleSlide} from './single';
+import { SingleSlide } from "./single";
 
 const INIT = [];
 
 export class Picker extends PureComponent {
   static defaultProps = {
-    dataSourse: [], //data
-    onceChange: arr => {}, // once change callback
-    confirm: arr => {}, //confirm  send data back
+    dataSource: [], //data
+    onceChange: (arr) => {}, // once change callback
+    confirm: (arr) => {}, //confirm  send data back
     cancel: () => {},
   };
 
@@ -31,12 +31,12 @@ export class Picker extends PureComponent {
   }
 
   componentDidMount() {
-    const {dataSourse} = this.props;
-    if (!dataSourse || !this.props.dataSourse.length) {
-      console.warn('SlidePicker: dataSource should be a array');
+    const { dataSource } = this.props;
+    if (!dataSource || !this.props.dataSource.length) {
+      console.warn("SlidePicker: dataSource should be a array");
       return;
     } else {
-      this.splitData(dataSourse);
+      this.splitData(dataSource);
     }
   }
 
@@ -50,47 +50,47 @@ export class Picker extends PureComponent {
   /**  props  -----------------------------------END */
 
   /** START init update  ----------------------------------*/
-  splitData = pros => {
+  splitData = (pros) => {
     const level1List = pros.map(this.fliterProperty);
-    this.setState({level1List});
+    this.setState({ level1List });
     this.setL2List(level1List[0].id);
   };
 
   // update Level2
-  setL2List = proId => {
+  setL2List = (proId) => {
     this.level2Ref && this.level2Ref.resetTrans();
-    const pros = this.props.dataSourse;
-    const L1Target = pros.find(pro => pro.id === proId);
+    const pros = this.props.dataSource;
+    const L1Target = pros.find((pro) => pro.id === proId);
     this.addToLocal(L1Target, 0);
 
     if (L1Target && L1Target.list) {
       const l2List = L1Target.list;
-      const level2List = l2List.map(item => ({
+      const level2List = l2List.map((item) => ({
         id: item.id,
         // pid: item.pid,
         name: item.name,
         list: item.list,
       }));
-      this.setState({level2List}, () => this.setL3List(level2List[0].id));
+      this.setState({ level2List }, () => this.setL3List(level2List[0].id));
     }
   };
 
   // update Level3
-  setL3List = cityId => {
+  setL3List = (cityId) => {
     this.level3Ref && this.level3Ref.resetTrans();
     const level2List = this.state.level2List;
-    const L2Target = level2List.find(item => item.id === cityId);
+    const L2Target = level2List.find((item) => item.id === cityId);
     this.addToLocal(L2Target, 1);
 
     if (L2Target && L2Target.list) {
       const l3List = L2Target.list;
       const list = l3List.map(this.fliterProperty);
-      this.setState({level3List: list});
+      this.setState({ level3List: list });
       this.addToLocal(list[0], 2);
     }
   };
 
-  fliterProperty = item => ({
+  fliterProperty = (item) => ({
     id: item.id,
     name: item.name,
   });
@@ -99,7 +99,7 @@ export class Picker extends PureComponent {
 
   // add to this.result   /level index from zero
   addToLocal = (todoItem, level) => {
-    const {list, ...final} = todoItem;
+    const { list, ...final } = todoItem;
     this.result[level] = final;
     // console.info('result', this.result);
     this.onceDataChange();
@@ -107,28 +107,28 @@ export class Picker extends PureComponent {
 
   /** START after choose ---------------------------------*/
 
-  doneL1 = proIndex => {
+  doneL1 = (proIndex) => {
     const target = this.state.level1List[proIndex];
     this.setL2List(target.id);
     this.addToLocal(target, 0);
   };
 
-  doneL2 = cityIndex => {
+  doneL2 = (cityIndex) => {
     const target = this.state.level2List[cityIndex];
     this.setL3List(target.id);
     this.addToLocal(target, 1);
   };
 
-  doneL3 = areaIndex => {
+  doneL3 = (areaIndex) => {
     const target = this.state.level3List[areaIndex];
     this.addToLocal(target, 2);
   };
 
   /**  after choose ---------------------------------  END*/
 
-  setLv1Ref = prosref => (this.level1Ref = prosref);
-  setLv2Ref = cityref => (this.level2Ref = cityref);
-  setL3Ref = arearef => (this.level3Ref = arearef);
+  setLv1Ref = (prosref) => (this.level1Ref = prosref);
+  setLv2Ref = (cityref) => (this.level2Ref = cityref);
+  setL3Ref = (arearef) => (this.level3Ref = arearef);
 
   render() {
     return (
@@ -179,20 +179,20 @@ const sts = StyleSheet.create({
   com: {
     // flex: 1, paddingTop: 20
   },
-  rest: {flex: 1, backgroundColor: '#fff'},
+  rest: { flex: 1, backgroundColor: "#fff" },
   btns: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: '#fff',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    backgroundColor: "#fff",
   },
   btn: {
     // backgroundColor: '#a00',
     padding: 15,
   },
-  btn_text: {fontSize: 18, color: '#4169E1'},
+  btn_text: { fontSize: 18, color: "#4169E1" },
   all: {
-    flexDirection: 'row',
-    backgroundColor: '#a00',
-    overflow: 'hidden',
+    flexDirection: "row",
+    backgroundColor: "#a00",
+    overflow: "hidden",
   },
 });
